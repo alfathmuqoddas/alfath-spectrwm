8370c085ce821895c4ea14b11f19fa6b814a7700
Alfath Muqoddas <alfath.muqoddas@gmail.com>
2024-06-29 05:49:16 +0700

    update

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt install spectrwm suckless-tools thunar xfce4-terminal lightdm ligthdm-gtk-greeter lxappearance xfce4-settings materia-gtk-theme
+
+git clone https://github.com/alfathmuqoddas/fonts-cool
+
+sudo cp -r fonts-cool/* /usr/share/fonts
+
+cp .spectrmw.con /home/{$USER}/.spectrwm.conf
+
+cp baraction.sh /home/{$USER}/baraction.sh
+
