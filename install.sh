4ad2e6b626f6f029eabec722a6a900ebbbac2c10
Alfath Muqoddas <85182650+alfathmuqoddas@users.noreply.github.com>
2024-07-21 22:55:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,11 @@ git clone https://github.com/alfathmuqoddas/fonts-cool
 
 sudo cp -r fonts-cool/* /usr/share/fonts
 
-cp .spectrmw.conf /home/{$USER}/.spectrwm.conf
+cp .spectrmw.conf /home/$USER/.spectrwm.conf
 
-cp baraction.sh /home/{$USER}/baraction.sh
+cp baraction.sh /home/$USER/baraction.sh
 
-sudo chmod u+x /home/{$USER}/baraction.sh
+sudo chmod u+x /home/$USER/baraction.sh
 
 sudo systemctl enable lightdm
 
