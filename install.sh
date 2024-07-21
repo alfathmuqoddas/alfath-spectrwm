5dfb04a3f207f06926c5cbc4fd4b17a1c251b76f
Alfath Muqoddas <85182650+alfathmuqoddas@users.noreply.github.com>
2024-07-21 22:30:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt install spectrwm suckless-tools thunar xfce4-terminal lightdm ligthdm-gtk-greeter lxappearance xfce4-settings materia-gtk-theme
+sudo apt install spectrwm suckless-tools thunar xfce4-terminal lightdm ligthdm-gtk-greeter lxappearance xfce4-settings materia-gtk-theme pulseaudio xsecurelock
 
 git clone https://github.com/alfathmuqoddas/fonts-cool
 
